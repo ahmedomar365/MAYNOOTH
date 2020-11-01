1c0b26fa536e5f00c4f6732423f1a86742f165a6
unknown <ahmedomarar365@gmail.com>
2020-11-01 20:27:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { PopularCategoriesComponent } from './popular-categories/popular-categories.component';
+import { OnSaleComponent } from './on-sale/on-sale.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    PopularCategoriesComponent,
+    OnSaleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
